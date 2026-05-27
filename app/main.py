1cf785b40ba26c75002a6ff579eb4440974355d8
nguyencon2309 <connoone0923@gmail.com>
2026-05-27 22:16:45 +0700

    add default route

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from app.routes import food, history, food101
 
 app = FastAPI(title="Food Backend API")
-
+@app.get("/")
+def read_root():
+    return {"message": "Backend SmartTray"}
 app.include_router(food.router, prefix="/foods", tags=["Foods"])
 app.include_router(history.router, prefix="/history", tags=["History"])
 app.include_router(food101.router, prefix="/food101", tags=["Food101"])
