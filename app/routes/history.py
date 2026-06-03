3a1fd50885d89d94ef9a905b021af11fc9a334e4
nguyencon2309 <connoone0923@gmail.com>
2026-06-04 01:07:07 +0700

    not match func api

--- app/routes/history.py
+++ app/routes/history.py
@@ -13,7 +13,7 @@ async def add(file:UploadFile = File(...), data: str=Form(...),current_user: dic
 @router.get("/id")
 def get(id: str,current_user: dict = Depends(check_token_middleware)):
     user_id = str(current_user.get("_id"))
-    return history_service.get_history(id, user_id)
+    return history_service.get_history_by_id(id, user_id)
 @router.get("/",dependencies=[Depends(check_token_middleware)])
 def get(current_user: dict = Depends(check_token_middleware)):
     user_id = str(current_user.get("_id"))
